16396ad79fa3a66b662e60b5924ca53a50f029e9
david1437 <davidsherrier1997@gmail.com>
2017-10-21 03:34:03 -0400

    firebase authentificaion

---
+++ app/src/main/java/com/example/junior/finfun/SignupActivity.java
@@ -0,0 +1,138 @@
+package com.example.junior.finfun;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by david on 10/13/17.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText emailInput, passwordInput;
+    private Button btnLogin, btnRegister;
+    private FirebaseAuth auth;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+        FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(SignupActivity.this, MainActivity.class));
+            finish();
+        }
+
+        auth = FirebaseAuth.getInstance();
+
+        emailInput = (EditText) findViewById(R.id.email_text);
+        passwordInput = (EditText) findViewById(R.id.password_text);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(getApplicationContext(),"Email field cant be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(getApplicationContext(),"Password field cant be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(getApplicationContext(),"Email field cant be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(getApplicationContext(),"Password field cant be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.length() < 6)
+                {
+                    Toast.makeText(getApplicationContext(),"Password field must have 6 or more characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
+                                    ref.child(user.getUid()).setValue(user.getEmail().toString());
+                                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else
+                                {
+                                    Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        auth.signOut();
+    }
+}
